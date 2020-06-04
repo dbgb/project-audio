ad3e8f51531e4a8d6f184923b5549841726c66c2
dbgb <dbayliss.mail@gmail.com>
2020-06-04 16:27:31 +0100

    Optimize top-level import load times in dev envionment

---
+++ client/.babelrc.js
@@ -0,0 +1,25 @@
+const plugins = [
+  [
+    // Allow babel to optimize top-level import load times in dev environment
+    // via tree-shaking (Occurs by default for production builds)
+    // re: https://material-ui.com/guides/minimizing-bundle-size/#option-2
+    "babel-plugin-import",
+    {
+      libraryName: "@material-ui/core",
+      libraryDirectory: "esm",
+      camel2DashComponentName: false,
+    },
+    "core",
+  ],
+  [
+    "babel-plugin-import",
+    {
+      libraryName: "@material-ui/icons",
+      libraryDirectory: "esm",
+      camel2DashComponentName: false,
+    },
+    "icons",
+  ],
+];
+
+module.exports = { plugins };
