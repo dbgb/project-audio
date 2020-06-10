2adcdb4357a152d161273f473de082999196845f
dbgb <dbayliss.mail@gmail.com>
2020-06-10 14:27:29 +0100

    Implement skeleton Profile component

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,27 +1,7 @@
 import React from "react";
-import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
-
-const ME_QUERY = gql`
-  {
-    me {
-      id
-      username
-      email
-      dateJoined
-    }
-  }
-`;
 
 function App() {
-  const { loading: meLoading, error: meError, data: meData } = useQuery(
-    ME_QUERY
-  );
-
-  if (meLoading) return <p>Loading...</p>;
-  if (meError) return <p>{meError}</p>;
-
-  return <div className="App">{JSON.stringify(meData)}</div>;
+  return <div className="App">App goes here</div>;
 }
 
 export default App;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const ME_QUERY = gql`
+  {
+    me {
+      id
+      username
+      email
+      dateJoined
+    }
+  }
+`;
 
 export default function Profile() {
-    return (
-        <div>
-            Profile goes here!
-        </div>
-    )
+  const { loading: meLoading, error: meError, data: meData } = useQuery(
+    ME_QUERY
+  );
+
+  if (meLoading) return <p>Loading...</p>;
+  if (meError) return <p>{meError}</p>;
+
+  return <div className="App">{JSON.stringify(meData)}</div>;
 }
