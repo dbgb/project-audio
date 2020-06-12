d97720571b18bbc9e6a876adf358ada2536c963e
dbgb <dbayliss.mail@gmail.com>
2020-06-12 12:41:09 +0100

    Update profile page layout

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import Loading from "../components/Shared/Loading";
+import Error from "../components/Shared/Error";
 
 export default function Profile() {
   // Component state
-  const { loading: meLoading, error: meError, data: meData } = useQuery(
-    ME_QUERY
+  const { loading, error, data } = useQuery(CURRENT_USER);
+
+  if (loading) return <Loading />;
+  else if (error) return <Error error={error} />;
+
+  return (
+    <div className="App">
+      <div>{`Welcome ${data.me.username}! (id: ${data.me.id})`}</div>
+      <div>Email: {data.me.email}</div>
+      <div>Date Joined: {data.me.dateJoined}</div>
+    </div>
   );
-
-  if (meLoading) return <p>Loading...</p>;
-  if (meError) return <p>{meError}</p>;
-
-  return <div className="App">{JSON.stringify(meData)}</div>;
 }
 
-// Queries
-const ME_QUERY = gql`
-  {
+const CURRENT_USER = gql`
+  query {
     me {
       id
       username
