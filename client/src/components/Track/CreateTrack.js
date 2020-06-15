6313b33372c1dc2a6dd8265e73e2d571f2d61b33
dbgb <dbayliss.mail@gmail.com>
2020-06-15 11:33:51 +0100

    Display floating action button for track creation

--- client/src/components/Track/CreateTrack.js
+++ client/src/components/Track/CreateTrack.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Fab } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 
 export default function CreateTrack() {
+  // Hook into MUI stylesheet
+  const classes = useStyles();
+
   return (
     <div>
-      Create
+      {/* Use floating action button to present track creation as the primary action for the page */}
+      <Fab
+        className={classes.fab}
+        color="primary"
+        size="medium"
+        variant="extended"
+      >
+        <Add className={classes.fabIcon} />
+        Add Track
+      </Fab>
+      {/* Creation dialog */}
     </div>
-  )
+  );
 }
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: "fixed",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  fabIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
