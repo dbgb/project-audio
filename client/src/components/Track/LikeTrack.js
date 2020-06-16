3a930252f56ec18e7a039eac092f83e6eb6e5f1d
dbgb <dbayliss.mail@gmail.com>
2020-06-16 20:14:18 +0100

    Add skeleton LikeTrack component

--- client/src/components/Track/LikeTrack.js
+++ client/src/components/Track/LikeTrack.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { FavoriteBorder } from "@material-ui/icons";
 
 export default function LikeTrack() {
+  // Hook into MUI stylesheet
+  const classes = useStyles();
+
   return (
     <div>
-      Like
+      <FavoriteBorder className={classes.icon} />
     </div>
-  )
+  );
 }
+
+// MUI component styling
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+}));
