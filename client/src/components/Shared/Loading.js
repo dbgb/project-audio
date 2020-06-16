d9ff18be82f3ea8672c4f5644e39d0d958309639
dbgb <dbayliss.mail@gmail.com>
2020-06-16 19:30:58 +0100

    Make Loading component size assignable via prop

--- client/src/components/Shared/Loading.js
+++ client/src/components/Shared/Loading.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { CircularProgress } from "@material-ui/core";
 
-export default function Loading() {
-  return <CircularProgress />;
+export default function Loading({ size = 40 }) {
+  return <CircularProgress size={size} />;
 }
