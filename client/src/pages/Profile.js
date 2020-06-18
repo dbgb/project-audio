62506bf03c59a744e27d50370a91ed26db07f7e9
dbgb <dbayliss.mail@gmail.com>
2020-06-18 09:15:00 +0100

    Display join date in localized, human readable format

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import moment from "moment";
 import Loading from "../components/Shared/Loading";
 import Error from "../components/Shared/Error";
 
@@ -11,11 +12,14 @@ export default function Profile() {
   if (loading) return <Loading />;
   else if (error) return <Error error={error} />;
 
+  // Display date in human readable format, localized to user computer timezone
+  let dateJoined = moment(data.me.dateJoined).format("MMMM Do YYYY, h:mm:ss a");
+
   return (
     <div className="App">
       <div>{`Welcome ${data.me.username}! (id: ${data.me.id})`}</div>
       <div>Email: {data.me.email}</div>
-      <div>Date Joined: {data.me.dateJoined}</div>
+      <div>Date Joined: {dateJoined}</div>
     </div>
   );
 }
