a0a1aae2bc83d264aef14ef5361a1c35b7501675
dbgb <dbayliss.mail@gmail.com>
2020-07-02 18:15:38 +0100

    Add Heroku buildpack prereq files

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python backend/manage.py migrate
+web: gunicorn --chdir backend app.wsgi —-log-file -
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -30,7 +30,7 @@ FRONTEND_DIR = os.path.abspath(os.path.join(BACKEND_DIR, "..", "client"))
 # https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Default to off if not switched on in os.environ
