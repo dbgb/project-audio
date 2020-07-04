02241a984b5edec2ba835da4635f2f64e0b75c3d
dbgb <dbayliss.mail@gmail.com>
2020-07-04 12:28:12 +0100

    Add build tasks script to Heroku prebuild command
    This update triggers a client build, then organises the static files so that they are ready for collection by Django.

--- build_tasks.sh
+++ build_tasks.sh
@@ -2,16 +2,18 @@
 
 # 1. Create production build
 cd client
+yarn install
 yarn build
 
 # 2. Organise files to be collected and served by Django at client root
 mkdir -p build/root
 cd build
 ROOT_FILES="*.ico *.js *.json *.png robots.txt"
-mv $ROOT_FILES root/
+mv -v $ROOT_FILES root/
 cd ../..
 
 # 3. Collect static files for backend
-pushd backend
-pipenv run python manage.py collectstatic --no-input
-popd
+# (Handled automatically by Heroku python buildpack in production)
+# pushd backend
+# pipenv run python manage.py collectstatic --no-input
+# popd
