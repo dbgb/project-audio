e1aa605b43dbd2f987890814a70fc2912a23581a
dbgb <dbayliss.mail@gmail.com>
2020-07-09 17:35:32 +0100

    Avoid stale cache read on Uploads page listing

--- client/src/pages/Uploads.js
+++ client/src/pages/Uploads.js
@@ -1,34 +1,32 @@
 import React, { Fragment } from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import { useParams } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Loading from "../components/Shared/Loading";
 import Error from "../components/Shared/Error";
 import TrackList from "../components/Track/TrackList";
-import { CURRENT_USER } from "../Root";
 
 export default function Uploads() {
   // Hook into MUI stylesheet
   const classes = useStyles();
 
-  // Component state
-  const { loading: userLoading, error: userError, data: userData } = useQuery(
-    CURRENT_USER
-  );
-  const {
-    loading: tracksLoading,
-    error: tracksError,
-    data: tracksData,
-  } = useQuery(USER_TRACKS, { variables: { id: userData.me.id } });
+  // Access user id from url params
+  const { id } = useParams();
 
-  if (userLoading || tracksLoading) return <Loading />;
-  if (userError || tracksError) {
-    let error = userError ? userError : tracksError;
+  const { loading, error, data } = useQuery(USER_TRACKS, {
+    variables: { id: id },
+    // Use cache, but also send network request to ensure up to date data
+    fetchPolicy: "cache-and-network",
+  });
+
+  if (loading) return <Loading />;
+  else if (error) {
     return <Error error={error} />;
   }
 
-  const userTracks = tracksData.user.trackSet;
+  const userTracks = data.user.trackSet;
 
   // Render component
   return (
@@ -36,7 +34,7 @@ export default function Uploads() {
       <div className={classes.root}>
         <div className={classes.container}>
           <Typography className={classes.title} variant="h5">
-            {userData.me.username}'s Uploads
+            {data.user.username}'s Uploads
           </Typography>
           {userTracks.length < 1 && (
             <Typography className={classes.emptyMessage}>
@@ -54,6 +52,7 @@ export default function Uploads() {
 const USER_TRACKS = gql`
   query($id: Int!) {
     user(id: $id) {
+      username
       trackSet {
         id
         title
