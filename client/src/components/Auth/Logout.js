456b4742ca6c4752970645dd2d6c00ba4445ca93
dbgb <dbayliss.mail@gmail.com>
2020-07-14 12:30:47 +0100

    Reset domain path on logout

--- client/src/components/Auth/Logout.js
+++ client/src/components/Auth/Logout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { useApolloClient } from "@apollo/react-hooks";
 import { Button, Typography } from "@material-ui/core";
 import { ExitToApp } from "@material-ui/icons";
@@ -9,9 +10,12 @@ import { ExitToApp } from "@material-ui/icons";
 export default function Logout() {
   // Hook into Apollo client state to allow direct write
   const client = useApolloClient();
+  const history = useHistory();
 
   // Handlers
   const handleSubmit = () => {
+    // Reset domain path
+    history.push("/");
     // Remove JWT from client
     localStorage.removeItem("authToken");
     // Then, update Apollo client state to reflect this change
--- client/src/components/Auth/index.js
+++ client/src/components/Auth/index.js
@@ -3,9 +3,12 @@ import Register from "./Register";
 import Login from "./Login";
 
 /**
- * Send user to register/login, or redirect to main page if authenticated
+ * Send user to register/login, or trigger redirect to main page if
+ * authenticated
  */
 export default () => {
+  // Index file in project root monitors client login state and controls whether
+  // auth, or main app are rendered
   const [isExistingUser, setIsExistingUser] = useState(true);
 
   return isExistingUser ? (
